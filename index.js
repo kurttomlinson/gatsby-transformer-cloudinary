627ee5760b0abf7099f2421dfe4cd063070719dc
Kurt Tomlinson <ktomlinson@taskrabbit.com>
2020-07-06 11:30:50 -0400

    gitpkg

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.getFixedImageObject = require('./get-image-objects').getFixedImageObject;
+exports.getFluidImageObject = require('./get-image-objects').getFluidImageObject;
+exports.createRemoteImageNode = require('./create-remote-image-node').createRemoteImageNode;
