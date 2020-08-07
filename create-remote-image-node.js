615e13f8aa72d5d9898ff1984e489a11219e8026
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-08-07 19:02:19 -0400

    gitpkg

---
+++ create-remote-image-node.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const { uploadImageToCloudinary } = require('./upload');
+const { createImageNode } = require('./create-image-node');
+const { getPluginOptions } = require('./options');
+
+exports.createRemoteImageNode = async ({
+  url,
+  overwriteExisting,
+  parentNode,
+  relationshipName,
+  createContentDigest,
+  createNode,
+  createNodeId,
+  reporter,
+}) => {
+  if (!reporter) {
+    throw Error(
+      "`reporter` is a required argument. It's available at `CreateNodeArgs.reporter`.",
+    );
+  }
+  if (!url) {
+    reporter.panic(
+      '`url` is a required argument. Pass the URL where the image is currently hosted so it can be downloaded by Cloudinary.',
+    );
+  }
+  if (!parentNode) {
+    reporter.panic(
+      "`parentNode` is a required argument. This parameter is used to link a newly created node representing the image to a parent node in Gatsby's GraphQL layer.",
+    );
+  }
+  if (!relationshipName) {
+    reporter.panic(
+      "`relationshipName` is a required argument. This parameter sets the name of the relationship between the parent node and the newly created node for this image in Gatsby's GraphQL layer.",
+    );
+  }
+  if (!createContentDigest) {
+    reporter.panic(
+      "`createContentDigest` is a required argument. It's available at `CreateNodeArgs.createContentDigest`.",
+    );
+  }
+  if (!createNode) {
+    reporter.panic(
+      "`createNode` is a required argument. It's available at `CreateNodeArgs.actions.createNode`.",
+    );
+  }
+  if (!createNodeId) {
+    reporter.panic(
+      "`createNodeId` is a required argument. It's available at `CreateNodeArgs.createNodeId`.",
+    );
+  }
+
+  const overwrite =
+    overwriteExisting == null
+      ? getPluginOptions().overwriteExisting
+      : overwriteExisting;
+
+  const publicId = path.parse(url).name;
+
+  const cloudinaryUploadResult = await uploadImageToCloudinary({
+    url,
+    publicId,
+    overwrite,
+    reporter,
+  });
+
+  const imageNode = createImageNode({
+    cloudinaryUploadResult,
+    parentNode,
+    createContentDigest,
+    createNodeId,
+  });
+
+  // Add the new node to Gatsby’s data layer.
+  createNode(imageNode, { name: 'gatsby-transformer-cloudinary' });
+
+  // Tell Gatsby to add `${relationshipName}` to the parent node.
+  const relationshipKey = `${relationshipName}___NODE`;
+  parentNode[relationshipKey] = imageNode.id;
+  return imageNode;
+};
