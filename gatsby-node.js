25fe7f4e64828604e0cb5800db28652546d9af14
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-10-04 12:26:05 -0400

    gitpkg

---
+++ gatsby-node.js
@@ -0,0 +1,213 @@
+const fs = require('fs-extra');
+const {
+  getFixedImageObject,
+  getFluidImageObject,
+} = require('./get-image-objects');
+const { uploadImageNodeToCloudinary } = require('./upload');
+const { setPluginOptions } = require('./options');
+const { createImageNode } = require('./create-image-node');
+const {
+  createAssetNodesFromData,
+} = require('./gatsby-node/create-asset-nodes-from-data');
+
+const ALLOWED_MEDIA_TYPES = ['image/png', 'image/jpeg', 'image/gif'];
+
+exports.onPreExtractQueries = async ({ store, getNodesByType }) => {
+  const program = store.getState().program;
+
+  // Check if there are any ImageSharp nodes. If so add fragments for ImageSharp.
+  // The fragment will cause an error if there are no ImageSharp nodes.
+  if (getNodesByType(`CloudinaryAsset`).length == 0) {
+    return;
+  }
+
+  // We have CloudinaryAsset nodes so let’s add our fragments to .cache/fragments.
+  await fs.copy(
+    require.resolve(`./fragments.js`),
+    `${program.directory}/.cache/fragments/cloudinary-asset-fragments.js`,
+  );
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type CloudinaryAsset implements Node @dontInfer {
+      fixed(
+        base64Width: Int
+        base64Transformations: [String!]
+        chained: [String!]
+        height: Int
+        transformations: [String!]
+        width: Int
+      ): CloudinaryAssetFixed!
+
+      fluid(
+        base64Width: Int
+        base64Transformations: [String!]
+        chained: [String!]
+        maxWidth: Int
+        transformations: [String!]
+      ): CloudinaryAssetFluid!
+    }
+
+    type CloudinaryAssetFixed {
+      aspectRatio: Float
+      base64: String!
+      height: Float
+      src: String
+      srcSet: String
+      width: Float
+    }
+
+    type CloudinaryAssetFluid {
+      aspectRatio: Float!
+      base64: String!
+      presentationHeight: Float
+      presentationWidth: Float
+      sizes: String!
+      src: String!
+      srcSet: String!
+    }
+  `);
+};
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    CloudinaryAsset: {
+      fixed: {
+        type: 'CloudinaryAssetFixed!',
+        resolve: (
+          {
+            public_id,
+            version,
+            cloudName,
+            originalHeight,
+            originalWidth,
+            defaultBase64,
+          },
+          {
+            base64Width,
+            base64Transformations,
+            ignoreDefaultBase64,
+            height,
+            width,
+            transformations,
+            chained,
+          },
+        ) =>
+          getFixedImageObject({
+            public_id,
+            version,
+            cloudName,
+            originalHeight,
+            originalWidth,
+            height,
+            width,
+            base64Width,
+            base64Transformations,
+            defaultBase64,
+            ignoreDefaultBase64,
+            transformations,
+            chained,
+          }),
+      },
+      fluid: {
+        type: 'CloudinaryAssetFluid!',
+        resolve: (
+          {
+            public_id,
+            version,
+            cloudName,
+            originalHeight,
+            originalWidth,
+            breakpoints,
+          },
+          {
+            base64Width,
+            base64Transformations,
+            maxWidth,
+            transformations,
+            chained,
+          },
+        ) =>
+          getFluidImageObject({
+            public_id,
+            version,
+            cloudName,
+            maxWidth,
+            breakpoints,
+            originalHeight,
+            originalWidth,
+            base64Width,
+            base64Transformations,
+            transformations,
+            chained,
+          }),
+      },
+    },
+  };
+
+  createResolvers(resolvers);
+};
+
+async function createAssetNodeFromFile({
+  node,
+  actions: { createNode, createParentChildLink },
+  createNodeId,
+  createContentDigest,
+  reporter,
+}) {
+  if (!ALLOWED_MEDIA_TYPES.includes(node.internal.mediaType)) {
+    return;
+  }
+
+  const cloudinaryUploadResult = await uploadImageNodeToCloudinary({
+    node,
+    reporter,
+  });
+
+  const imageNode = createImageNode({
+    cloudinaryUploadResult,
+    parentNode: node,
+    createContentDigest,
+    createNode,
+    createNodeId,
+  });
+
+  // Add the new node to Gatsby’s data layer.
+  createNode(imageNode);
+
+  // Tell Gatsby to add `childCloudinaryAsset` to the parent `File` node.
+  createParentChildLink({
+    parent: node,
+    child: imageNode,
+  });
+
+  return imageNode;
+}
+
+exports.onCreateNode = async ({
+  node,
+  actions,
+  createNodeId,
+  createContentDigest,
+  reporter,
+}) => {
+  createAssetNodesFromData({
+    node,
+    actions,
+    createNodeId,
+    createContentDigest,
+    reporter,
+  });
+  await createAssetNodeFromFile({
+    node,
+    actions,
+    createNodeId,
+    createContentDigest,
+    reporter,
+  });
+};
+
+exports.onPreInit = ({ reporter }, pluginOptions) => {
+  setPluginOptions({ pluginOptions, reporter });
+};
