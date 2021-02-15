090a37d9f6f785b05dd4474029805a49030ca9fd
Kurt Tomlinson <kurttomlinson@gmail.com>
2021-02-15 11:28:35 -0500

    gitpkg

---
+++ create-image-node.js
@@ -0,0 +1,86 @@
+const stringify = require('fast-json-stable-stringify');
+const { getPluginOptions } = require('./options');
+
+function getDefaultBreakpoints(imageWidth) {
+  const {
+    breakpointsMaxImages,
+    fluidMinWidth,
+    fluidMaxWidth,
+  } = getPluginOptions();
+
+  const max = Math.min(imageWidth, fluidMaxWidth);
+  const min = fluidMinWidth;
+
+  if (max <= min) {
+    return [max];
+  }
+
+  const breakpoints = [max];
+  for (let i = 1; i < breakpointsMaxImages; i++) {
+    const breakpoint = max - (i * (max - min)) / (breakpointsMaxImages - 1);
+    breakpoints.push(Math.round(breakpoint));
+  }
+  return breakpoints;
+}
+
+exports.createImageNode = ({
+  cloudinaryUploadResult: {
+    responsive_breakpoints,
+    public_id,
+    version,
+    height,
+    width,
+  },
+  parentNode,
+  createContentDigest,
+  createNodeId,
+  cloudName,
+  defaultBase64,
+  defaultTracedSVG,
+}) => {
+  let breakpoints = getDefaultBreakpoints(width);
+  if (
+    responsive_breakpoints &&
+    responsive_breakpoints[0] &&
+    responsive_breakpoints[0].breakpoints &&
+    responsive_breakpoints[0].breakpoints.length > 0
+  ) {
+    breakpoints = responsive_breakpoints[0].breakpoints.map(
+      ({ width }) => width,
+    );
+  }
+
+  const fingerprint = stringify({
+    cloudName,
+    height,
+    public_id,
+    breakpoints,
+    version,
+    width,
+  });
+
+  const imageNode = {
+    // These helper fields are only here so the resolvers have access to them.
+    // They will *not* be available via Gatsby’s data layer.
+    cloudName: cloudName || getPluginOptions().cloudName,
+    public_id: public_id,
+    version: version,
+    originalHeight: height,
+    originalWidth: width,
+    breakpoints,
+    defaultBase64,
+    defaultTracedSVG,
+
+    // Add the required internal Gatsby node fields.
+    id: createNodeId(`CloudinaryAsset-${fingerprint}`),
+    parent: parentNode.id,
+    internal: {
+      type: 'CloudinaryAsset',
+      // Gatsby uses the content digest to decide when to reprocess a given
+      // node. We can use the Cloudinary URL to avoid doing extra work.
+      contentDigest: createContentDigest(fingerprint),
+    },
+  };
+
+  return imageNode;
+};
